1107a8c757c4aeaaa126332a28a8a4eb9fff90b2
amandarangel <50452552+amandarangel@users.noreply.github.com>
2019-08-13 20:10:52 -0300

    mensagem boas vindas

--- script.js
+++ script.js
@@ -2,17 +2,32 @@
 function ValidateEmail (InputText)
 {
   var mailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   if(InputText.value.match(mailFormat))
   {
       alert("O email " +  (document.getElementById('email-placeholder').value) + " foi enviado.");
-      document.form1.email.focus();
-      return true; 
-     
-  }
-  else{
-    alert("Email inválido");
-    document.form1.email.focus();
-    return false;
+      document.form1.email;
+                
+      document.getElementById("bemvindo").innerHTML = 'Bem-vindo (a) ' + (document.getElementById('email-placeholder').value) + '!';
+      
+      return true;
+  }   
+   
+  else
+  {
+      alert("Email inválido");
+      document.form1.email;
+      location.reload();
+      return false;
   }
 
 }
+  
+function setMessage(msg)
+{
+    message.textContent = msg;
+} 
+
+
+
+
